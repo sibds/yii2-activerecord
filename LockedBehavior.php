4d1f2ae93bc07a9001a36f0a0a8657dd13d03ecb
Vadim Mazur <mazurva@gmail.com>
2016-03-11 08:52:03 +0500

    Update LockedBehavior.php

--- LockedBehavior.php
+++ LockedBehavior.php
@@ -1,15 +1,16 @@
 <?php
 /**
-     * Created by PhpStorm.
-     * User: vadim
-     * Date: 05.02.16
-     * Time: 12:57
-     */
+ * Created by PhpStorm.
+ * User: vadim
+ * Date: 05.02.16
+ * Time: 12:57
+ */
 
 namespace sibds\components;
 
 
 use yii\base\Behavior;
+use yii\base\Exception;
 
 class LockedBehavior extends Behavior
 {
@@ -29,27 +30,32 @@ class LockedBehavior extends Behavior
     {
         $owner = $this->owner;
         if ($owner->hasAttribute($this->lockedAttribute)) {
-                    if (empty($owner->{$this->lockedAttribute}) || is_null($owner->{$this->lockedAttribute})) {
-                                    $owner->{$this->lockedAttribute} = $this->valueUnlock;
-                    }
+            if (empty($owner->{$this->lockedAttribute}) || is_null($owner->{$this->lockedAttribute})) {
+                $owner->{$this->lockedAttribute} = $this->valueUnlock;
+            }
         }
     }
 
     public function lock()
     {
-        /* @var $owner BaseActiveRecord */
+        /* @var $owner ActiveRecord */
         $owner = $this->owner;
 
         $owner->{$this->lockedAttribute} = $this->valueLock;
+        if(!$owner->validate())
+            throw new \yii\db\Exception('Error saving model! Validating not comlete: ' . implode('; ', $owner->firstErrors));
         $owner->save();
     }
 
     public function unlock()
     {
-        /* @var $owner BaseActiveRecord */
+        /* @var $owner ActiveRecord */
         $owner = $this->owner;
 
         $owner->{$this->lockedAttribute} = $this->valueUnlock;
+        if(!$owner->validate())
+            throw new \yii\db\Exception('Error saving model! Validating not comlete: ' . implode('; ', $owner->firstErrors));
+
         $owner->save();
     }
 }
